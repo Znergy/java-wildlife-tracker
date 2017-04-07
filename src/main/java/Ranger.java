c4f11113452042db68a641e7f27a2f5dfca6bbdf
Ryan <ryanjonesirl@gmail.com>
2017-04-07 09:25:02 -0700

    add interface methods to the Ranger class as well as static find/static all methods

--- src/main/java/Ranger.java
+++ src/main/java/Ranger.java
@@ -4,7 +4,70 @@ public class Ranger implements RangerDuties {
   private String name;
   private String phone_number;
 
+  public Ranger(int badge_number, String name, String phone_number) {
+    this.id = id;
+    this.badge_number = badge_number;
+    this.name = name;
+    this.phone_number = phone_number;
+  }
 
+  public String getName() {
+    return this.name;
+  }
+
+  public int getBadgeNumber() {
+    return this.badge_number;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public String getPhoneNumber() {
+    return this.phone_number;
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO rangers (badge_number, name, phone_number) VALUES (:badge_number, :name, :phone_number);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("badge_number", this.badge_number)
+        .addParameter("name", this.name)
+        .addParameter("phone_number", this.phone_number)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public void update() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE rangers SET (badge_number=:badge_number, name=:name, phone_number=:phone_number) WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .addParameter("badge_number", this.badge_number)
+        .addParameter("name", this.name)
+        .addParameter("phone_number", this.phone_number)
+        .executeUpdate();
+    }
+  }
+
+  public static List<Ranger> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM rangers;";
+      return con.createQuery(sql)
+        .executeAndFetch(Ranger.class);
+    }
+  }
+
+  public static Ranger find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM rangers WHERE id=:id;";
+      Ranger ranger = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Ranger.class);
+      return ranger;
+    }
+  }
 
 
 }
--- src/main/java/RangerDuties.java
+++ src/main/java/RangerDuties.java
@@ -1,10 +1,8 @@
 public interface RangerDuties {
-
   public String getName();
-  public String getBadgeNumber();
-  public String getId();
+  public int getBadgeNumber();
+  public int getId();
   public String getPhoneNumber();
   public void save();
-  public void delete();
-  public void updateAll();
+  public void update();
 }
